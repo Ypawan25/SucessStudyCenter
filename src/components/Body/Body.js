2e3e9b42c7582a6f95317555ec93f9ccce7a534e
Pawan Kumar Yadav <impawankryadav@gmail.com>
2023-03-25 16:53:40 +0530

    al done

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,8 +3,29 @@ import chil from "./chil.jpeg"
 export default function Body(){
  return(
     <div className="container" >
-    <img className="images" src={chil} alt="childphoto"/>
+    <img className="images" src={chil} alt="childphoto" />
+    <marquee><h4>NOW OPEN FOR REGISTRATION FOR YEAR 2023. WE ALSO PROVIDE HOME TUTOR</h4></marquee>
 
+    <div className="details">
+        <ul> Our Program Includes
+            <li>Class 8 - 12</li>
+            <li>Science ,Commerce, Arts</li>
+            <li>Group Study</li>
+            <li>Intensive Learing</li>
+            <li>Excellent Teachers</li>
+            <li>Individual Plans</li>
+           
+        </ul>
+        <ul> Contact Details
+            <li>Mobile : 7007753991</li>
+            <li>Address : 15/220 Indra Nagar Lucknow Near Dabel College</li>
+           
+            <li>Er. Amit Kumar Singh</li>
+           
+        </ul>
+
+    </div>
+    <marquee><h4>WE TEACH AS PER ACDEMIC AND NATIONAL EDUCATION POLICY BY MINISTRY OF EDUCATION , GOVERNMENT OF INDIA</h4></marquee>
     </div >
  )   
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,7 @@ import logo from "../logo1.jpeg"
 export default function Navbar() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg  nabvar">
   <div className="container-fluid">
     <a className="navbar-brand logoNav" href="#navbar"><img src={logo} alt="logo" /> </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
